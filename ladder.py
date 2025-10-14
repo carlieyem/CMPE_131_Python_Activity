426d34da69897e52e2dc899b2f07be1469b16cd5
Carlie Yem <CarlieYem@gmail.com>
2025-10-14 13:28:21 -0700

    ladder my_steps func

--- ladder.py
+++ ladder.py
@@ -1,7 +1,14 @@
-def main():
-    def my_steps(n):
-        if (n < 1) or (n >25):
-            return 0, "ValueError"
-        return n
-        
-main()
+def my_steps(n):
+    if (n < 1) or (n >25):
+        return 0, "ValueError"
+    a = 1
+    b = 2
+    for i in range(3, n+1):
+        if i == 1:
+            return 1
+        if i == 2:
+            return 2
+        else:
+            a = b
+            b = a + b
+    return b
