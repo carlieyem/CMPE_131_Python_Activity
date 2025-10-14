2d401729414fb975b72b6410f9260f73d756ba17
Carlie Yem <CarlieYem@gmail.com>
2025-10-14 15:49:43 -0700

    range and return message oopsies

--- ladder.py
+++ ladder.py
@@ -1,8 +1,7 @@
 def my_steps(n: int) -> int:
-    if not isinstance(n, int) or (n not in range(1, 25)):
-        raise TypeError("Input must be an integer")
+    if not isinstance(n, int) or (n not in range(1, 26)):
+        raise TypeError("Input must be an integer and in range 1-25")
     
     if n <= 2:
         return n
-    for i in range(3, n+1):
-        return my_steps(n-1) + my_steps(n-2)
+    return my_steps(n-1) + my_steps(n-2)
