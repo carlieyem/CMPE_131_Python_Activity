f94b260e14c8824d4eb4b0c3abf105d465683f5f
Carlie Yem <CarlieYem@gmail.com>
2025-10-14 15:51:07 -0700

    .... HUH

--- ladder.py
+++ ladder.py
@@ -1,7 +1,7 @@
 def my_steps(n: int) -> int:
-    if not isinstance(n, int) or (n not in range(1, 26)):
-        raise TypeError("Input must be an integer and in range 1-25")
-    
-    if n <= 2:
+    if (not isinstance(n, int)) or (n not in range(1, 26)):
+        raise ValueError("n must be an integer between 1 and 25 inclusive")
+
+    if n <= 2: 
         return n
-    return my_steps(n-1) + my_steps(n-2)
+    return my_steps(n - 1) + my_steps(n - 2)
