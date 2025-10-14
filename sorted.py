6b5529c3f4519325df59c5acd1a2e40af4fc5557
Carlie Yem <CarlieYem@gmail.com>
2025-10-14 15:11:32 -0700

    re sorted lol

--- sorted.py
+++ sorted.py
@@ -1,3 +1,18 @@
+def sort_dic(dictionary):
+    for i in range(0, len(dictionary)):
+        for j in range(1, len(dictionary) - i):
+            if dictionary[j-1][0] < dictionary[j][0]:
+                temp = dictionary[j-1]
+                dictionary[j-1] = dictionary[j]
+                dictionary[j] = temp
+    return dictionary
+
 def reverse_sort_dictionary(dictionary):
-    sorted_dict = sorted(dictionary.items(), key=lambda item: item[0], reverse = True)
-    return [(name, number[0] for name, data in sorted_dict)]
+    sorted_dict = []
+    
+    for key in dictionary:
+        tupleValue = dictionary[key]
+        sorted_dict.append((key, tupleValue[0]))
+
+    sorted_dict = sort_dic(sorted_dict)
+    return sorted_dict
