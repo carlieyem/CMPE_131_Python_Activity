446c4fca590a4e54a2a7c2964020c016c8ef3248
Carlie Yem <CarlieYem@gmail.com>
2025-10-14 13:59:46 -0700

    bug1 file fixed.... hopefully

---
+++ bug1.py
@@ -0,0 +1,35 @@
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+    def shape(self):
+        return "This is a circle"
+# TODO: there's code missing in one or more lines below
+
+class Circle(Base):
+    def __init__(x, y, size):
+        super().__init__(x, y, size)
+    def shape(self):
+        return Base.shape(self) + "\n (" + self.x + ", " + self.y + ")\n" + self.size
+    def draw(self):
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+      , '              ' ,
+    ,                     ,
+   ,                       ,
+  ,                         ,
+  ,                         ,
+  ,                         ,
+   ,                       ,
+    ,                    ,
+      ,               , '
+       ' - , _ _ _ , '
+"""
+def main():
+    c = Circle(1,2,3)
+    print(c.shape())
+    print(c.draw())
+main()
