ca1659e865c7a15fe95c9fd0587c6ebb713af3ac
Carlie Yem <CarlieYem@gmail.com>
2025-10-14 13:40:55 -0700

    sorted confused what it meant by reverse

--- sorted.py
+++ sorted.py
@@ -1,3 +1,3 @@
 def reverse_sort_dictionary(dictionary):
-    reversed_dict = sorted(dictionary.items(), key: None, reverse = True)
-    return reversed_dict
+    sorted_dict = sorted(dictionary.items(), key=lambda item: item[0], reverse = True)
+    return [(name, number[0] for name, data in sorted_dict)]
